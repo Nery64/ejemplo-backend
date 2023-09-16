7844d47fee122391cb13a4357ab01fa608ee0a08
Nery <newymejia@gmail.com>
2023-09-16 00:01:44 -0600

    cuarto.

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,11 +1,51 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
     
+constructor (private readonly ProductService: ProductService){}
+
     @Get()
-    getProducts(){
-        return 
+        getProducts(){
+            return this.ProductService.findAll();)
+        }
+
+
+    //READ
+    @Get(':id')
+    getProductById(@Param('id', ParseIntPipe) id: number){
+
+        console.log(`el id a buscar es ${id}`)
+        return this.ProductService.findById(id);
+    }
+
+    //UPDATE
+    @Put(':id')
+    updateProductById(@Param('id', ParseIntPipe) id: number, @Body() body){
+
+        console.log(`el id a actualizar es ${id}`)
+        console.log(body);
+        return this.ProductService.updateProducts(body, id);
+
+    }
+
+    //DELETE
+    @Delete(':id')
+    deleteProductById(@Param('id', ParseIntPipe) id: number){
+
+        console.log(`el id a eliminar es ${id}`)
+        return this.ProductService.deleteProducts(id);
+    }
+
+    //CREATE
+    @Post()
+    createProductById(@Body() body){
+
+        
+        console.log(body);
+        return this.ProductService.createProducts(body);
+
     }
 
 }
