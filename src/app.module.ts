541c2cbadd05e842174a4fc415ef8137cc667f87
Nery <newymejia@gmail.com>
2023-09-15 23:01:44 -0600

    s_commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ClientModule } from './client/client.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ClientModule],
+  imports: [ClientModule, ProductModule],
   controllers: [],
   providers: [],
   exports: []
